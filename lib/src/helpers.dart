358db5d81e4d02345fc6fb3be961622ba0474856
Yilmaz Guleryuz <397125+zeusbaba@users.noreply.github.com>
2021-03-04 10:30:19 +0100

    Null Safety!

--- lib/src/helpers.dart
+++ lib/src/helpers.dart
@@ -9,10 +9,10 @@ shift(List l) {
   return null;
 }
 
-Map merge(Map obj, defaults) {
+Map merge(Map? obj, defaults) {
   if (obj == null) {
     obj = new Map();
   }
-  defaults.forEach((key, val) => obj.putIfAbsent(key, () => val));
+  defaults.forEach((key, val) => obj!.putIfAbsent(key, () => val));
   return obj;
 }
